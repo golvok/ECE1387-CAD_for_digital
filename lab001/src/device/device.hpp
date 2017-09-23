8e819591a8c50d2a54a72c27c1338af8fca3b189
Matthew J.P. Walker <walkerm930@gmail.com>
2017-09-23 09:41:41 -0400

    move netlist back to it's file

--- lab001/src/device/device.hpp
+++ lab001/src/device/device.hpp
@@ -62,23 +62,6 @@ public:
 	}
 };
 
-template<typename NODE_ID>
-class Netlist {
-public:
-	Netlist()
-		: connections()
-	{ }
-
-	void add_connection(const NODE_ID& source, const NODE_ID& sink) {
-		connections[source].emplace(sink);
-	}
-
-	auto begin() const { return std::begin(connections); }
-	auto end()   const { return   std::end(connections); }
-private:
-	std::unordered_map<NODE_ID,std::unordered_set<NODE_ID>> connections;
-};
-
 } // end namespace util
 
 namespace std {
---
+++ lab001/src/util/netlist.hpp
@@ -0,0 +1,24 @@
+
+#include <unordered_map>
+#include <unordered_set>
+
+namespace util {
+
+template<typename NODE_ID>
+class Netlist {
+public:
+	Netlist()
+		: connections()
+	{ }
+
+	void add_connection(const NODE_ID& source, const NODE_ID& sink) {
+		connections[source].emplace(sink);
+	}
+
+	auto begin() const { return std::begin(connections); }
+	auto end()   const { return   std::end(connections); }
+private:
+	std::unordered_map<NODE_ID,std::unordered_set<NODE_ID>> connections;
+};
+
+} // end namespace util
