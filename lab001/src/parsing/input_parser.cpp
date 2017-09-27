48839cfebc68e62e92b34b8e2e23cf4b54430146
Matthew J.P. Walker <walkerm930@gmail.com>
2017-09-27 01:14:19 -0400

    device bbox now correct

--- lab001/src/parsing/input_parser.cpp
+++ lab001/src/parsing/input_parser.cpp
@@ -61,7 +61,7 @@ boost::variant<ParseResult, std::string> parse_data(std::istream& is) {
 	using std::end;
 
 	device::DeviceInfo device_info;
-	device_info.bounds = geom::BoundBox<int>(0,0,get<0>(parse_results),get<0>(parse_results));
+	device_info.bounds = geom::BoundBox<int>(0,0,get<0>(parse_results)-1,get<0>(parse_results)-1);
 	device_info.track_width = get<1>(parse_results);
 	device_info.pins_per_block_side = 1;
 	device_info.num_blocks_adjacent_to_channel = 2;
