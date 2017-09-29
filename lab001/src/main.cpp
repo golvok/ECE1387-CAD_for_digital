e8f32bd732b0cbcfcf7d8cf336d36790f7ed19b4
Matthew J.P. Walker <walkerm930@gmail.com>
2017-09-28 20:12:02 -0400

    add visualization of fanout

--- lab001/src/main.cpp
+++ lab001/src/main.cpp
@@ -54,22 +54,32 @@ int program_main(const std::string& data_file_name) {
 		},
 		[&](const parsing::input::ParseResult& pr) {
 			do_optional_input_data_dump(data_file_name, pr);
-			device::FullyConnectedConnector connector(pr.device_info);
-			const device::Device<device::FullyConnectedConnector> dev(
+			device::WiltonConnector connector(pr.device_info);
+			const device::Device<device::WiltonConnector> dev(
 				pr.device_info, connector
 			);
 
 			const auto gfx_state_keeper = graphics::get().fpga().pushState(&dev);
 			graphics::get().waitForPress();
 
-			device::RouteElementID test_re(
-				util::make_id<device::XID>((int16_t)2),
-				util::make_id<device::YID>((int16_t)4),
-				(int16_t)0
-			);
-			dout(DL::INFO) << test_re << '\n';
-			for (const auto& fanout : dev.fanout(test_re)) {
-				dout(DL::INFO) << '\t' << fanout << '\n';
+			for (int i = 0; i < dev.info().track_width*2; ++i) {
+				device::RouteElementID test_re(
+					util::make_id<device::XID>((int16_t)2),
+					util::make_id<device::YID>((int16_t)4),
+					(int16_t)i
+				);
+
+				std::unordered_map<device::RouteElementID, graphics::t_color> colours;
+				colours[test_re] = graphics::t_color(0,0xFF,0);
+
+				dout(DL::INFO) << test_re << '\n';
+				for (const auto& fanout : dev.fanout(test_re)) {
+					colours[fanout] = graphics::t_color(0xFF,0,0);
+					dout(DL::INFO) << '\t' << fanout << '\n';
+				}
+
+				const auto gfx_state_keeper = graphics::get().fpga().pushState(&dev, std::move(colours));
+				graphics::get().waitForPress();
 			}
 
 			const auto result = algo::route_all(pr.pin_to_pin_netlist, dev);
