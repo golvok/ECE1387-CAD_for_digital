c39a0e3ee85e04654e1b54b18f5d9dde4af55243
Matthew J.P. Walker <walkerm930@gmail.com>
2017-10-28 18:02:16 -0400

    add AtomID

--- src/device/common_ids.hpp
+++ src/device/common_ids.hpp
@@ -4,6 +4,7 @@
 #include <util/id.hpp>
 
 #include <cstdint>
+#include <iostream>
 
 namespace device {
 
@@ -16,6 +17,24 @@ using BlockPinID = util::ID<std::int16_t, BlockPinIDTag>;
 
 using XYIDPair = std::pair<XID,YID>;
 
+struct AtomIDTag { static const std::uint32_t DEFAULT_VALUE = 0x80008000; };
+using AtomID = util::ID<std::uint32_t, AtomIDTag>;
+
+struct XValTag { constexpr static const double DEFAULT_VALUE = -123.45; };
+struct YValTag { constexpr static const double DEFAULT_VALUE = -123.45; };
+using XVal = util::ID<double, XValTag>;
+using YVal = util::ID<double, YValTag>;
+
+inline std::ostream& operator<<(std::ostream& os, const AtomID& id) {
+	os << "Atom:" << id.getValue();
+	return os;
+}
+
+}
+
+namespace std {
+	template<typename> struct hash;
+	template<> struct hash<device::AtomID> : util::MyHash<device::AtomID>::type { };
 }
 
 #endif // DEVICE__COMMON_IDS_H
