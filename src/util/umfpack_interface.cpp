06be54dbdbb619e840e2133cb6fb253229cd9d8e
Matthew J.P. Walker <walkerm930@gmail.com>
2017-10-29 13:28:57 -0400

    c++ify the solve code

--- src/util/umfpack_interface.cpp
+++ src/util/umfpack_interface.cpp
@@ -3,23 +3,57 @@
 #include <util/logging.hpp>
 
 int solve() {
-	int    n = 5 ;
-	int    Ap [ ] = {0, 2, 5, 9, 10, 12} ;
-	int    Ai [ ] = { 0,  1,  0,   2,  4,  1,  2,  3,   4,  2,  1,  4} ;
-	double Ax [ ] = {2., 3., 3., -1., 4., 4., -3., 1., 2., 2., 6., 1.} ;
-	double b [ ] = {8., 45., -3., 3., 19.} ;
-	double x [5] ;
+	int    n_row = 5;
+	int    n_col = n_row;
+	std::vector<int> column_starts{0, 2, 5, 9, 10, 12};
+	std::vector<int> row_indexes{ 0,  1,  0,   2,  4,  1,  2,  3,   4,  2,  1,  4};
+	std::vector<double> a_values{2., 3., 3., -1., 4., 4., -3., 1., 2., 2., 6., 1.};
+	std::vector<double> b_vector{8., 45., -3., 3., 19.};
+	std::vector<double> x_vector(n_row);
 
-	double *null = (double *) NULL ;
-	int i ;
-	void *Symbolic, *Numeric ;
-	(void) umfpack_di_symbolic (n, n, Ap, Ai, Ax, &Symbolic, null, null) ;
-	(void) umfpack_di_numeric (Ap, Ai, Ax, Symbolic, &Numeric, null, null) ;
-	umfpack_di_free_symbolic (&Symbolic) ;
-	(void) umfpack_di_solve (UMFPACK_A, Ap, Ai, Ax, x, b, Numeric, null, null) ;
-	umfpack_di_free_numeric (&Numeric) ;
-	for (i = 0 ; i < n ; i++) {
-		dout(DL::INFO) << "x [" << i << "] = " << x[i] << '\n' ;
+	void *symbolic_data;
+	umfpack_di_symbolic(
+		n_row,
+		n_col,
+		column_starts.data(),
+		row_indexes.data(),
+		a_values.data(),
+		&symbolic_data,
+		nullptr,
+		nullptr
+	);
+
+	void *numeric_data;
+	umfpack_di_numeric(
+		column_starts.data(),
+		row_indexes.data(),
+		a_values.data(),
+		symbolic_data,
+		&numeric_data,
+		nullptr,
+		nullptr
+	);
+
+	umfpack_di_free_symbolic(&symbolic_data);
+
+	umfpack_di_solve(
+		UMFPACK_A,
+		column_starts.data(),
+		row_indexes.data(),
+		a_values.data(),
+		x_vector.data(),
+		b_vector.data(),
+		numeric_data,
+		nullptr,
+		nullptr
+	);
+
+	umfpack_di_free_numeric(&numeric_data);
+
+	int i = 0;
+	for (const auto& x : x_vector) {
+		dout(DL::INFO) << "x [" << i << "] = " << x << '\n';
+		i++;
 	}
 
 	return 0;
