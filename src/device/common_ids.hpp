78337dad960ad6100206386c2d8a2975f3393db9
Matthew J.P. Walker <walkerm930@gmail.com>
2017-10-28 22:16:09 -0400

    move BlockID to common_ids

--- src/device/common_ids.hpp
+++ src/device/common_ids.hpp
@@ -1,7 +1,9 @@
 #ifndef DEVICE__COMMON_IDS_H
 #define DEVICE__COMMON_IDS_H
 
+#include <util/bit_tools.hpp>
 #include <util/id.hpp>
+#include <util/print_printable.hpp>
 
 #include <cstdint>
 #include <iostream>
@@ -30,10 +32,37 @@ inline std::ostream& operator<<(std::ostream& os, const AtomID& id) {
 	return os;
 }
 
+struct BlockIDTag { static const std::uint32_t DEFAULT_VALUE = 0x80008000; };
+class BlockID : public util::ID <std::uint32_t, BlockIDTag>, public util::print_printable {
+	using ID::ID;
+public:
+	template<typename T>
+	explicit BlockID(T&& t) : util::ID <std::uint32_t, BlockIDTag>(std::forward<T>(t)) { } // ECF compiler seems to need this???
+	explicit BlockID() : util::ID <std::uint32_t, BlockIDTag>() { } // ECF compiler seems to need this???
+	using ID::IDType;
+private:
+	static IDType makeValueFromXY(XID x, YID y) { return (util::no_sign_ext_cast<IDType>(x.getValue()) << 16) | util::no_sign_ext_cast<IDType>(y.getValue()); }
+	static XYIDPair xyFromValue(IDType id)      { return {xFromValue(id), yFromValue(id)}; }
+	static XID xFromValue(IDType id)            { return util::make_id<XID>(util::no_sign_ext_cast<XID::IDType>(id >> 16)); }
+	static YID yFromValue(IDType id)            { return util::make_id<YID>(util::no_sign_ext_cast<YID::IDType>(id & 0x0000FFFF)); }
+
+public:
+	BlockID(XID x, YID y) : ID(makeValueFromXY(x,y)) { }
+
+	auto getX() const { return xFromValue(getValue()); }
+	auto getY() const { return yFromValue(getValue()); }
+
+	template<typename STREAM>
+	void print(STREAM& os) const {
+		os << "{x" << getX().getValue() << ",y" << getY().getValue() << '}';
+	}
+};
+
 }
 
 namespace std {
 	template<typename> struct hash;
+	template<> struct hash<device::BlockID> : util::MyHash<device::BlockID>::type { };
 	template<> struct hash<device::AtomID> : util::MyHash<device::AtomID>::type { };
 }
 
--- src/device/device.hpp
+++ src/device/device.hpp
@@ -13,32 +13,6 @@
 
 namespace device {
 
-struct BlockIDTag { static const std::uint32_t DEFAULT_VALUE = 0x80008000; };
-class BlockID : public util::ID <std::uint32_t, BlockIDTag>, public util::print_printable {
-	using ID::ID;
-public:
-	template<typename T>
-	explicit BlockID(T&& t) : util::ID <std::uint32_t, BlockIDTag>(std::forward<T>(t)) { } // ECF compiler seems to need this???
-	explicit BlockID() : util::ID <std::uint32_t, BlockIDTag>() { } // ECF compiler seems to need this???
-	using ID::IDType;
-private:
-	static IDType makeValueFromXY(XID x, YID y) { return (util::no_sign_ext_cast<IDType>(x.getValue()) << 16) | util::no_sign_ext_cast<IDType>(y.getValue()); }
-	static XYIDPair xyFromValue(IDType id)      { return {xFromValue(id), yFromValue(id)}; }
-	static XID xFromValue(IDType id)            { return util::make_id<XID>(util::no_sign_ext_cast<XID::IDType>(id >> 16)); }
-	static YID yFromValue(IDType id)            { return util::make_id<YID>(util::no_sign_ext_cast<YID::IDType>(id & 0x0000FFFF)); }
-
-public:
-	BlockID(XID x, YID y) : ID(makeValueFromXY(x,y)) { }
-
-	auto getX() const { return xFromValue(getValue()); }
-	auto getY() const { return yFromValue(getValue()); }
-
-	template<typename STREAM>
-	void print(STREAM& os) const {
-		os << "{x" << getX().getValue() << ",y" << getY().getValue() << '}';
-	}
-};
-
 struct PinGIDTag { static const std::uint64_t DEFAULT_VALUE = 0x8000800080008000; };
 class PinGID : public util::ID<std::uint64_t, PinGIDTag>, public util::print_printable {
 	using ID::ID;
@@ -275,7 +249,6 @@ private:
 
 namespace std {
 	template<typename> struct hash;
-	template<> struct hash<device::BlockID> : util::MyHash<device::BlockID>::type { };
 	template<> struct hash<device::PinGID> : util::MyHash<device::PinGID>::type { };
 	template<> struct hash<device::RouteElementID> : util::MyHash<device::RouteElementID>::type { };
 }
