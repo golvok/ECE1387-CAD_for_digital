99267a0a38cea318dd71967d01ce04a0e4605132
Matthew J.P. Walker <walkerm930@gmail.com>
2017-11-23 18:48:59 -0500

    try mutation approach. 5.9% faster

--- branch-and-bound/src/sat_maxer_main.cpp
+++ branch-and-bound/src/sat_maxer_main.cpp
@@ -128,7 +128,7 @@ namespace std {
 	};
 }
 
-struct Visitor : public util::DefaultGraphVisitor<Graph::Vertex> {
+struct Visitor {
 	const CNFExpression& expression;
 	CNFEvaluation<std::vector> evaluator;
 	int num_partial_settings_explored = 0;
@@ -140,17 +140,18 @@ struct Visitor : public util::DefaultGraphVisitor<Graph::Vertex> {
 		, evaluator(expression)
 	{ }
 
-	template<typename StateStack>
-	void onExplore(const Graph::Vertex& vertex, const StateStack& state_stack) {
-		// dout(DL::INFO) << vertex.literal() << ' ';
-		const auto& new_counts = evaluator.addSetting(vertex.literal().id(), !vertex.inverted);
+	template<typename Vertex, typename StateStack>
+	void onExplore(const Vertex& vertex, const StateStack& state_stack) {
+		// dout(DL::INFO) << vertex.literal << ' ';
+		const auto& new_counts = evaluator.addSetting(vertex.literal.id(), !vertex.literal.inverted());
 		(void)new_counts;
 		(void)state_stack;
 	}
 
-	void onLeave(const Graph::Vertex& vertex) {
-		const auto& new_counts = evaluator.removeSetting(vertex.literal().id(), !vertex.inverted);
-		// dout(DL::INFO) << vertex.literal() << ": leavecounts={fc=" << new_counts.false_count << ", uc=" << new_counts.undecidable_count << "}\n";
+	template<typename Vertex>
+	void onLeave(const Vertex& vertex) {
+		const auto& new_counts = evaluator.removeSetting(vertex.literal.id(), !vertex.literal.inverted());
+		// dout(DL::INFO) << vertex.literal << ": leavecounts={fc=" << new_counts.false_count << ", uc=" << new_counts.undecidable_count << "}\n";
 		(void)new_counts;
 	}
 
@@ -178,7 +179,7 @@ struct Visitor : public util::DefaultGraphVisitor<Graph::Vertex> {
 			num_partial_settings_explored += 1;
 		}
 
-		if ((num_partial_settings_explored + num_complete_settings_explored) % 500000 == 0) {
+		if ((num_partial_settings_explored + num_complete_settings_explored) % 10000000 == 0) {
 			dout(DL::INFO) << "Status: ";
 			printExploredMessage(dout(DL::INFO));
 			dout(DL::INFO) << "\n";
@@ -191,7 +192,7 @@ struct Visitor : public util::DefaultGraphVisitor<Graph::Vertex> {
 	template<typename Cost, typename StateStack>
 	void onNewBest(const Cost& best_cost, const StateStack& state_stack) {
 		best_solution.clear();
-		std::transform(begin(state_stack), end(state_stack), std::back_inserter(best_solution), [=](auto&& s) { return s.vertex.literal(); });
+		std::transform(begin(state_stack), end(state_stack), std::back_inserter(best_solution), [=](auto&& s) { return s.vertex.literal; });
 		dout(DL::INFO) << "New best: cost=" << best_cost << " settings=";
 		util::print_container(best_solution, dout(DL::INFO));
 		dout(DL::INFO) << "\n";
@@ -220,8 +221,8 @@ struct GraphicsVisitor : Visitor {
 
 	bool did_graphics_init = false;
 
-	template<typename StateStack>
-	void onExplore(const Graph::Vertex& vertex, const StateStack& state_stack) {
+	template<typename Vertex, typename StateStack>
+	void onExplore(const Vertex& vertex, const StateStack& state_stack) {
 		Visitor::onExplore(vertex, state_stack);
 
 		if (not did_graphics_init) {
@@ -231,44 +232,36 @@ struct GraphicsVisitor : Visitor {
 
 		std::vector<Literal> path;
 		for (const auto& state : state_stack) {
-			path.push_back(state.vertex.literal());
+			path.push_back(state.vertex.literal);
 		}
 		graphics::get().tree().addPath(std::move(path));
 	}
 };
 
 void satisfy_maximally(const CNFExpression& expression) {
-	using ID = Graph::Vertex;
-	const auto graph = Graph{expression.all_literals()};
-	const auto initial_list = graph.roots();
-
-
-	GraphicsVisitor visitor { expression };
-	// Visitor visitor { expression };
+	// GraphicsVisitor visitor { expression };
+	Visitor visitor { expression };
 
 	struct State {
-		ID vertex;
+		struct Vertex {
+			Literal literal;
+		} vertex;
 		int parent_lower_bound;
 	};
 
-	std::vector<State> seed_states;
-	// for (const auto& id : initial_list) {
-	// 	seed_states.emplace_back(State{
-	// 		id;
-	// 	});
-	// }
-	seed_states.emplace_back(State{
-		*begin(initial_list),
-		-100,
-	});
-
 	auto best_cost = visitor.evalPartialSolution(std::vector<State>()).upper_bound;
 
-	for (const auto& seed_state : seed_states) {
-		std::vector<State> state_stack { seed_state, };
+	std::vector<State> state_stack;
+	for (const auto& lit_id : expression.all_literals()) {
+		state_stack.emplace_back(State{
+			Literal(false, lit_id),
+			-100
+		});
+	}
 
-		while (!state_stack.empty()) {
-			const auto& state = state_stack.back();
+	int state_stack_pos = 0;
+		while (state_stack_pos >= 0) {
+			const auto& state = state_stack[state_stack_pos];
 
 			visitor.onExplore(state.vertex, state_stack);
 			auto cost = visitor.evalPartialSolution(state_stack);
@@ -281,26 +274,23 @@ void satisfy_maximally(const CNFExpression& expression) {
 				}
 			}
 
-			auto new_state = State{
-				*begin(graph.fanout(state.vertex)),
-				cost.lower_bound,
-			};
-
-			if (skip_branch || !graph.isValid(new_state.vertex)) {
-				while (!state_stack.empty()) {
-					visitor.onLeave(state_stack.back().vertex);
-					if (state_stack.back().parent_lower_bound < best_cost and !state_stack.back().vertex.inverted) {
-						state_stack.back().vertex.inverted = true;
+			if (skip_branch || state_stack_pos == (int)state_stack.size() - 1) {
+				while (state_stack_pos >= 0) {
+					auto& new_current_state = state_stack[state_stack_pos];
+					visitor.onLeave(new_current_state.vertex);
+					if (new_current_state.parent_lower_bound < best_cost and !new_current_state.vertex.literal.inverted()) {
+						new_current_state.vertex.literal = Literal(true, new_current_state.vertex.literal.id());
 						break;
 					} else {
-						state_stack.pop_back();
+						state_stack_pos -= 1;
 					}
 				}
 			} else {
-				state_stack.push_back(new_state);
+				state_stack_pos += 1;
+				state_stack[state_stack_pos].vertex.literal = Literal(false, state_stack[state_stack_pos].vertex.literal.id());
+				state_stack[state_stack_pos].parent_lower_bound = cost.lower_bound;
 			}
 		}
-	}
 
 	visitor.onComplete();
 }
